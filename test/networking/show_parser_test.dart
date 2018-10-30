17208f8f41817d9334a61e3cb3213e7adc1c5d50
Andri Jasinski <andrjuwkoy@gmail.com>
2018-10-30 16:10:18 +0200

    Update show_parser_test.dart

--- test/networking/show_parser_test.dart
+++ test/networking/show_parser_test.dart
@@ -7,7 +7,6 @@ void main() {
     test('parsing test', () {
       List<Show> deserialized = ShowParser.parse(showsXml);
       expect(deserialized.length, 3);
-      kuku jopt
         
       var jumanji = deserialized.first;
       expect(jumanji.id, '1155306');
